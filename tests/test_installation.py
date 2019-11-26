5f97ca61793db4422b13fb926477fae943cc6827
Augusto <augusto.kiel@gmail.com>
2019-11-26 07:32:51 -0300

    Agrego definicion de constructor y actualizo las dependencias

---
+++ tests/test_installation.py
@@ -0,0 +1,9 @@
+try:
+    import bqplot
+    import ipycanvas
+    import ipyleaflet
+    import nbconvert
+    import sympy
+    print('Instalación exitosa.')
+except ImportError as e:
+    raise e
